20066b2b3a1ab5b863cffaa5d9bb3bf43b183cf7
Thaddeus Krugman <thaddeuskrugman@gmail.com>
2023-12-14 21:47:31 -0500

    .

--- src/routes/app/new-todo/+page.server.ts
+++ src/routes/app/new-todo/+page.server.ts
@@ -1,16 +1,29 @@
 import { fail, redirect } from '@sveltejs/kit';
+import { superValidate } from 'sveltekit-superforms/server';
+import newTodoSchema from '$lib/schemas/newTodoSchema.js';
+
+export const load = async () => {
+	const form = await superValidate(newTodoSchema);
+
+	return { form };
+};
 
 export const actions = {
 	default: async ({ request, locals: { supabase, getSession } }) => {
-		const formData = await request.formData();
-		const title = formData.get('title') as string;
-		const description = formData.get('description') as string;
-
 		const session = await getSession();
 
 		if (!session) {
 			throw redirect(303, '/');
 		}
+
+		const form = await superValidate(request, newTodoSchema);
+
+		if (!form.valid) {
+			return fail(400, form);
+		}
+
+		const title = form.data.title as string;
+		const description = form.data.description as string;
 
 		const { error } = await supabase.from('todos').insert({
 			title,
--- src/routes/app/new-todo/+page.svelte
+++ src/routes/app/new-todo/+page.svelte
@@ -1,16 +1,46 @@
 <script lang="ts">
-	import NewTodoForm from '$lib/components/elements/NewTodoForm.svelte';
 	import NoLongerSignedIn from '$lib/components/elements/NoLongerSignedIn.svelte';
-	export let form;
-	export let data;
+	import { superForm } from 'sveltekit-superforms/client';
+	import type { PageData } from './$types';
+	import newTodoSchema from '$lib/schemas/newTodoSchema';
+
+	export let data: PageData;
+
+	const { form, errors, enhance } = superForm(data.form, {
+		validators: newTodoSchema
+	});
 
 	let { supabase, session } = data;
 	$: ({ supabase, session } = data);
 </script>
 
 {#if session}
-	<NewTodoForm {form} />
+	<div class="mx-auto max-w-md w-full h-full flex flex-col items-center justify-center">
+		<h1 class="text-4xl sm:text-5xl md:text-5xl font-bold text-center">New Todo</h1>
+		<form class="flex flex-col w-full space-y-5 mt-8" method="post" use:enhance>
+			<input
+				class={`${$errors.title && 'input-invalid'} input`}
+				type="text"
+				name="title"
+				aria-invalid={$errors.title ? 'true' : undefined}
+				placeholder="Title"
+				bind:value={$form.title}
+			/>
+			{#if $errors.title}<span class="invalid">{$errors.title}</span>{/if}
+
+			<textarea
+				class={`${$errors.description && 'input-invalid'} textarea`}
+				rows="4"
+				name="description"
+				aria-invalid={$errors.description ? 'true' : undefined}
+				placeholder="Description"
+				bind:value={$form.description}
+			/>
+			{#if $errors.description}<span class="invalid">{$errors.description}</span>{/if}
+
+			<button class="btn variant-filled-primary">Submit</button>
+		</form>
+	</div>
 {:else}
 	<NoLongerSignedIn />
 {/if}
-```
