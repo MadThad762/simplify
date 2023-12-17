0157e0a220883d6b59f6b60329ec3e66707a3485
Thaddeus Krugman <thaddeuskrugman@gmail.com>
2023-12-17 05:22:43 -0500

    added functionality to the tab switcher

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -2,7 +2,6 @@
 	import NoLongerSignedIn from '$lib/components/elements/NoLongerSignedIn.svelte';
 	import RadioGroup from '$lib/components/elements/RadioGroup.svelte';
 	import TodoTable from '$lib/components/elements/TodoTable.svelte';
-	// @ts-ignore
 	import Plus from 'svelte-heros-v2/Plus.svelte';
 	export let data;
 
@@ -27,14 +26,18 @@
 					strokeWidth="3"
 				/></span
 			>
-			<RadioGroup {value} />
+			<RadioGroup bind:value />
 			<a href={'/app/new-todo'} class="btn-icon variant-filled-primary"
 				><Plus class={'h-6 w-6 outline-none focus:outline-none'} strokeWidth="3" /></a
 			>
 		</div>
 		<div class="relative w-full max-w-4xl mx-auto">
 			<div class="glow-bg absolute -inset-5 rounded-xl" />
-			<TodoTable {todos} />
+			{#if value === 0}
+				<TodoTable {todos} />
+			{:else if value === 1}
+				<div><p class="text-center py-20 text-2xl sm:text-3xl md:text-4xl">Coming Soon!</p></div>
+			{/if}
 		</div>
 	{:else}
 		<NoLongerSignedIn />
